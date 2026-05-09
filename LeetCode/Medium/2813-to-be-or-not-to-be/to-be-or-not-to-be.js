92c85e373ab6bf6964040436eaebcff5ada6a5df
Veer Gupta <veer27012005@gmail.com>
2026-05-09 11:17:30 +0530

    Time: 37 ms (88.44%) | Memory: 53 MB (85.18%) - LeetSync

---
+++ LeetCode/Medium/2813-to-be-or-not-to-be/to-be-or-not-to-be.js
@@ -0,0 +1,35 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    
+    function toBe(v){
+        if(v===val){
+            return true;
+        }
+        else{
+            throw new Error("Not Equal");
+        }
+    }
+
+    function notToBe(v){
+        if(v!==val){
+            return true;
+        }
+        else{
+            throw new Error("Equal");
+        }
+    }
+    
+   return {
+        toBe,
+        notToBe
+    };
+
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
